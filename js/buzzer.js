fa68cf2960916c9f08b12f741189e39544c601bc
dariuszewski <dariuszewski32@gmail.com>
2022-04-04 20:06:15 +0200

    change vibration

--- js/buzzer.js
+++ js/buzzer.js
@@ -2,16 +2,13 @@ function vibrate(ms) {
     navigator.vibrate(ms)
 }
 
-function vibratePattern(short, long) {
+function vibratePattern(short, long, pause) {
     // short (int) -> short vibration time in miliseconds
     // long (int) -> long vibration time in miliseconds
-    for (i=0; i<3; i++) {
-        vibrate(short)
-    };
-    for (i=0; i<3; i++) {
-        vibrate(long)
-    };
-    for (i=0; i<3; i++) {
-        vibrate(short)
-    };
+    // pause (int) -> pause between vibrations
+    navigator.vibrate([
+        short, pause, short, pause, short, pause,
+        long, pause, long, pause, long, pause,
+        short, pause, short, pause, short, pause
+    ])
 };
