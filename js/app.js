6830c94e922b6a30663356df949b176054ede52d
dariuszewski <dariuszewski32@gmail.com>
2022-04-06 21:03:43 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 if("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
         navigator.serviceWorker
-        .register("sw.js")
+        .register("/sw.js")
         // Promise syntax - promise means that at some point this will 
         // be finished succefully or not (asynchronous js)
         .then(res => console.log("Service Worked registered."))
